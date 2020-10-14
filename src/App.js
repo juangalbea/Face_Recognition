b15f016564a35dc0fa99aec5699586a9805d32ba
juangalbea <juangalbea@gmail.com>
2020-10-14 18:18:55 +0100

    deploy 2nd time heroku

--- src/App.js
+++ src/App.js
@@ -71,8 +71,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch('http://localhost:3000/imageurl', {
-      // fetch('https://dry-citadel-52850.herokuapp.com/imageurl', {
+      fetch('https://dry-citadel-52850.herokuapp.com/imageurl', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -81,8 +80,7 @@ class App extends Component {
     })
       .then(response => response.json())
       .then(response => {
-        fetch('http://localhost:3000/image', {
-          // fetch('https://dry-citadel-52850.herokuapp.com/image', {
+          fetch('https://dry-citadel-52850.herokuapp.com/image', {
           method: 'put',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
